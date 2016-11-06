187d322ba092a39d4eb23fdf715670c91e63d684
Heptagon196 <1176483765@qq.com>
2016-11-06 16:06:24 +0800

    Commit

--- judge.cpp
+++ judge.cpp
@@ -17,41 +17,34 @@ struct result{
     double time;
 };//Judge的结果
 
-//输出格式。
 const char Status[][40]={"Accepted","Wrong Answer","Compile Error","Time Limit Exceeded","Memory Limit Exceeded","Runtime Error"};
-const char Status_Color[]={green,red,yellow,red,red,yellow};
-char Dict[100][20]={"windows.h","system(","freopen(","fopen(","<con>","!"};
-//禁用单词。"!"标志数组的结束。
+const char Status_Color[]={green,red,yellow,red,red,yellow};//输出Accepted等提示信息的颜色
+char Dict[100][20]={"windows.h","system(","freopen(","fopen(","<con>","!"};//禁用单词。"!"标志数组的结束
 
 double timelimit=1.0;
 int memorylimit=128000;
 char name[512];
 
-void HighLight()
-{//高亮显示。
+void HighLight() {//高亮显示
     printf("\033[1m");
 }
 
-inline void ClearColor()
-{//清除颜色。
+inline void ClearColor() {//清除颜色
     printf("\033[0m");
 }
 
-inline void ClearFile()
-{//清理文件。
+inline void ClearFile() {//清理文件
     system("rm ./.ejudge.*");
 }
 
-void print(int st)
-{//输出Accepted等提示。高亮+彩显
+void print(int st) {//输出Accepted等提示。高亮+彩显
     color(Status_Color[st],black);
     HighLight();
     puts(Status[st]);
     ClearColor();
 }
 
-void PrintResult(result x)
-{//输出Judge结果
+void PrintResult(result x) {//输出Judge结果
     foreground(green);
     printf("Time:");
     foreground(yellow);
@@ -63,8 +56,7 @@ void PrintResult(result x)
     print(x.s);
 }
 
-void GetName()
-{//获取当前目录名
+void GetName() {//获取当前目录名
     system("basename $PWD > .ejudge.tmp");
     FILE *fp=fopen(".ejudge.tmp","r");
     fscanf(fp,"%s",name);
@@ -75,18 +67,15 @@ void GetName()
             return ;
 }
 
-void Args(int c,char *v[])
-{//解析命令行参数
+void Args(int c,char *v[]) {//解析命令行参数
     for (int i=1;i<c;i++)
-        if (v[i][0]=='-')
-        {
+        if (v[i][0]=='-') {
             i++;
             if (v[i-1][1]=='t')//时间
                 sscanf(v[i],"%lf",&timelimit);
             if (v[i-1][1]=='m')//内存
                 sscanf(v[i],"%d",&memorylimit);
-            if (v[i-1][1]=='w')//添加禁用单词，如-w3表示添加接下的的三个单词，-w与-w1等效。
-            {
+            if (v[i-1][1]=='w') {//添加禁用单词，如-w3表示添加接下的的三个单词，-w与-w1等效
                 int last,t;
                 for (last=0;Dict[last][0]!='!';last++) ;
                 if (sscanf(v[i-1],"-w%d",&t)==-1)
@@ -95,7 +84,8 @@ void Args(int c,char *v[])
                     sprintf(Dict[last+k],"%s",v[i]),i++;
                 Dict[last+t][0]='!';
             }
-        }else{
+        }
+        else{
             sprintf(name,"%s",v[i]);
             for (int k=strlen(name)-1;k>=0;k--)
                 if ((name[k]=='\n')||(name[k]=='\r'))
@@ -105,8 +95,7 @@ void Args(int c,char *v[])
         }
 }
 
-bool exist(char * filename)
-{//检测文件是否存在
+bool exist(char * filename) {//检测文件是否存在
     FILE *fp=fopen(filename,"r");
     if (fp==NULL)
         return false;
@@ -114,8 +103,7 @@ bool exist(char * filename)
     return true;
 }
 
-bool cmp(char *str,int s,char *word)
-{//比较字符串，从str的第s个和word的第1个字符开始比较
+bool cmp(char *str,int s,char *word) {//比较字符串，从str的第s个和word的第1个字符开始比较
     if (strlen(str)-s<strlen(word))
         return false;
     for (int i=0;i<strlen(word);i++)
@@ -124,14 +112,12 @@ bool cmp(char *str,int s,char *word)
     return true;
 }
 
-bool SafetyCheck()
-{//检测是否有禁用单词
+bool SafetyCheck() {//检测是否有禁用单词
     int r=false;
     char str[2048];
     sprintf(str,"%s.cpp",name);
     FILE *fp=fopen(str,"r");
-    if (fp==NULL)
-    {
+    if (fp==NULL) {
         foreground(red);
         HighLight();
         printf("Error:");
@@ -140,8 +126,7 @@ bool SafetyCheck()
         return true;
     }
     int line=0,ifndef=0,flag=0,ifdef=0;//忽略在注释和#ifndef EJUDGE和#ifdef EJUDGE #else中的单词
-    while (fgets(str,2000,fp)!=NULL)
-    {
+    while (fgets(str,2000,fp)!=NULL) {
         if (str[strlen(str)-1]=='\n')
             str[strlen(str)-1]=0;
         line++;
@@ -168,8 +153,7 @@ bool SafetyCheck()
                 continue;
             else if (cmp(str,i,(char *)"#include"))
                 include=1;
-            else if (str[i]=='"'&&include)//不允许调用自定义头文件
-            {
+            else if (str[i]=='"'&&include) {//不允许调用自定义头文件
                 foreground(red);
                 HighLight();
                 printf("Error:");
@@ -179,8 +163,7 @@ bool SafetyCheck()
             }
             else
                 for (int k=0;Dict[k][0]!='!';k++)
-                    if (cmp(str,i,Dict[k]))
-                    {
+                    if (cmp(str,i,Dict[k])) {
                         foreground(red);
                         HighLight();
                         printf("Error:");
@@ -193,19 +176,16 @@ bool SafetyCheck()
     return r;
 }
 
-bool Compile()
-{//编译程序
+bool Compile() {//编译程序
     puts("Compiling...");
-    if (SafetyCheck())
-    {
+    if (SafetyCheck()) {
         ClearFile();
         print(CE);
         return true;
     }
     char str[512];
     sprintf(str,"g++ %s.cpp -o %s -O2 -DEJUDGE",name,name);
-    if (WEXITSTATUS(system(str)))
-    {
+    if (WEXITSTATUS(system(str))) {
         print(CE);
         ClearFile();
         return true;
@@ -214,8 +194,7 @@ bool Compile()
     return false;
 }
 
-result judge(char *in,char *out)
-{//评测单个测试点
+result judge(char *in,char *out) {//评测单个测试点
     int s=AC,memo;
     double time;
     char str[512];
@@ -233,14 +212,12 @@ result judge(char *in,char *out)
 //      Time:0.02s Memory:1924KB
 //故有如下解析代码：
     fscanf(fp,"%c",&ch);
-    if (ch=='C')
-    {
+    if (ch=='C') {
         s=TLE;
         while (ch!='T')
             fscanf(fp,"%c",&ch);
     }
-    if (ch=='t')
-    {
+    if (ch=='t') {
         s=RE;
         while (ch!='T')
             fscanf(fp,"%c",&ch);
@@ -270,15 +247,13 @@ int main(int argc,char *argv[])
         return 0;
     int score=0,tot=0,st=0,maxmemo=0;
     double maxtime=0;
-    for (int i=0;i<=20;i++)
-    {
+    for (int i=0;i<=20;i++) {
         char in[512],out[512];
         sprintf(in,"%s%d.in",name,i);
         sprintf(out,"%s%d.out",name,i);
         if (!exist(in))
             continue;
-        if (!exist(out))
-        {
+        if (!exist(out)) {
             sprintf(out,"%s%d.ans",name,i);//如果不存在.out文件，尝试.ans后缀。
             if (!exist(out))
                 continue;
@@ -297,8 +272,7 @@ int main(int argc,char *argv[])
         maxtime=max(maxtime,tmp.time);
         maxmemo=max(maxmemo,tmp.memo);
     }
-    if (tot==0)
-    {
+    if (tot==0) {
         foreground(red);
         HighLight();
         printf("Error:");
